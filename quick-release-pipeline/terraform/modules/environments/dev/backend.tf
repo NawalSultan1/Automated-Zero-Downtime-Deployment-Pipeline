d445d637a53386841cf8a64a2a01ef82ab34a1ba
NawalSultan1 <nawalsultan2001@gmail.com>
2025-07-04 03:08:42 +0500

    backend

---
+++ quick-release-pipeline/terraform/modules/environments/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform{
+     backend "s3" {
+          backend = "nawalsultan123"
+          key = "C:\Users\Nawal Sultan\Desktop\Automated Zero-Downtime Deployment Pipeline\quick-release-pipeline\terraform\modules\environments\dev\terraform.tfstate"
+          region = "us-southeast-2"
+     }
+     
+}
