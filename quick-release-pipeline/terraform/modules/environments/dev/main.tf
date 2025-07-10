eae0cfbf36c9ea7387b648acdebc003cb64f0644
NawalSultan1 <nawalsultan2001@gmail.com>
2025-07-11 03:35:43 +0500

    key pairs to shh server

--- quick-release-pipeline/terraform/modules/environments/dev/main.tf
+++ quick-release-pipeline/terraform/modules/environments/dev/main.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   region = var.aws_region
 }
+resource "aws_key_pair" "blue" {             //here we are telling aws to create a box with the name key_name and in that key_name add this public key given below
+  key_name = var.key_name                    //this public key and the key name are given from variables (which act as an empty form) and tfvars file(which act as the answers to the empty form i.e variables.tf)
+  public_key = var.public_key
+}
+
+resource "aws_instance" "first_Server" {
+  ami = "ami-020cba7c55df1f615"
+  instance_type = var.instance_type
+  tags = {
+    Name= "First_server"
+    Project = "quick_release_pipeline"
+    Environment = "dev"
+  }
+}
--- quick-release-pipeline/terraform/modules/environments/dev/variables.tf
+++ quick-release-pipeline/terraform/modules/environments/dev/variables.tf
@@ -1,11 +1,19 @@
 variable "aws_region" {
   type = string
   description = "To define the region in which the resourses will form"
-  default = "us-southeast-2"
+  default = "us-east-1"
 }
 
 variable "instance_type" {
   description = "The size of the instances to be created"
   type = string
   default = "t2.micro"
+}
+variable "key_name" {
+  description = "The key to connect with the first server"
+  type = string
+}
+variable "public_key" {
+  description = "location of the public key"
+  type = string
 }
